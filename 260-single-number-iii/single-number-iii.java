bd6b73324bee2ba83782aed55cf34fbe9282f7c3
kumari1022 <practicecoading8@gmail.com>
2025-10-26 22:44:55 +0530

    Time: 1 ms (99.86%) | Memory: 46 MB (18.90%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -1,22 +1,27 @@
-class Solution {
-    public int[] singleNumber(int[] nums) {
-        int xor = 0;
-        for (int n : nums) {
+class Solution 
+{
+    public int[] singleNumber(int[] nums)
+     {
+        int xor=0;
+        for (int n:nums)
+         {
             xor ^= n;
         }
         
-        // Get rightmost set bit
-        int diff = xor & -xor;
+        int diff=xor&-xor;
         
-        int a = 0, b = 0;
-        for (int n : nums) {
-            if ((n & diff) == 0) {
-                a ^= n;
-            } else {
-                b ^= n;
+        int a=0,b=0;
+        for (int n:nums) 
+        {
+            if((n&diff)==0) {
+                a^= n;
+            }
+             else
+              {
+                b^= n;
             }
         }
         
-        return new int[]{a, b};
+        return new int[]{a,b};
     }
 }
